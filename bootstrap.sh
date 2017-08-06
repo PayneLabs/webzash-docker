3f518311e5cdc435b7083eaef55ad447c66c9c53
WMPayne <w.payne.fc@gmail.com>
2017-08-06 12:23:03 -0500

    update dockerfile for persistent mysql database and small fixes on apache side

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sed -i -e "s/DocumentRoot .*/DocumentRoot ${CAKEPHP_WEBROOT//\//\\\/}/" -e "s/<Directory .*>/<Directory ${CAKEPHP_WEBROOT//\//\\\/}>/" /etc/apache2/sites-available/000-default.conf
-
+rm -f /var/run/apache2/apache2.pid
 exec "$@"
